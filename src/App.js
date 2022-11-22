66a7c05ff6d42c09f9a77ece0d3e27a591b8d47a
nam334 <namratadas334@gmail.com>
2022-11-22 20:29:55 +0530

    added parel project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", { id: "heading" }, "Lets learn parcel");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+console.log(heading);
